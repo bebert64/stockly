21a5c485c1f53377af876b78afab1bc9fbf5d61a
Romain Cassan <romain.cassan@autajon.com>
2022-07-06 17:16:07 +0200

    Fixed for too many recursions

--- main.py
+++ main.py
@@ -4,24 +4,46 @@ arr1 = list(map(int, input().split()))
 
 def solution(shortcuts, nb_intersections):
     answers = [0] * nb_intersections
-    shortcuts_dict = to_dict(shortcuts)
+    try:
+        shortcuts_dict = to_dict(shortcuts)
+    except:
+        print("error to_dict")
+        exit()
     # print(shortcuts_dict)
     for i in range(1, nb_intersections):
         # print(f"checking {i=}")
         if i not in shortcuts_dict:
             # print(f"{i=} not an end point")
-            answers[i] = answers[i-1] + 1
+            try:
+                answers[i] = answers[i-1] + 1
+            except:
+                print("error in initial value")
+                exit()
             # print(f"{answers[i]=}")
         else:
-            origins = shortcuts_dict[i] + [i-1]
+            try:
+                origins = shortcuts_dict[i] + [i-1]
+            except:
+                print("error trying generate origins")
+                exit()
             try:
                 origins.remove(i)
             except ValueError:
                 pass
             # print(f"{origins=}")
-            answers[i] = min(answers[origin] for origin in origins) + 1
+            try:
+                answers[i] = min(answers[origin] for origin in origins) + 1
+            except:
+                print("error getting minimum")
+                exit()
+
+            try:
+                retro_check(answers, i)
+            except Exception as err:
+                print(err)
+                exit()
             # print(f"{answers[i]=}")
-            retro_check(answers, i)
+            
     return answers
 
 
@@ -41,14 +63,18 @@ def to_dict(shortcuts):
 
 
 def retro_check(answers, i):
-    if answers[i] <= answers[i-1] - 2:
-        # print("retro-check needed")
-        # print(f"{answers[i]=}")
-        # print(f"before : {answers[i-1]=}")
-        answers[i-1] = answers[i] + 1
-        # print(f"after : {answers[i-1]=}")
-        retro_check(answers, i-1)
-
+    assert i>0
+    assert i<nb_intersections
+    # if answers[i] <= answers[i-1] - 2:
+    #     # print("retro-check needed")
+    #     # print(f"{answers[i]=}")
+    #     # print(f"before : {answers[i-1]=}")
+    #     answers[i-1] = answers[i] + 1
+    #     # print(f"after : {answers[i-1]=}")
+    #     retro_check(answers, i-1)
+    while i > 0 and (answers[i] <= answers[i-1] - 2):
+        answers[i - 1] = answers[i] + 1
+        i -= 1
 
 answer = solution(arr1, nb_intersections)
 print(" ".join([str(i) for i in answer]))

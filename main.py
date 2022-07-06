8c7fa800f05e819e3bce2050d902677fce1341ec
Romain Cassan <romain.cassan@autajon.com>
2022-07-06 16:43:37 +0200

    First version, not optimized

---
+++ main.py
@@ -0,0 +1,56 @@
+arr1 = list(map(int, "2 2 3".split()))
+arr2 = list(map(int, "1 2 3 4 5".split()))
+arr3 = list(map(int, "4 4 4 4 7 7 7".split()))
+arr4 = list(map(int, "5 5 5 5 5 7 7".split()))
+
+
+def solution(shortcuts):
+    nb_intersections = len(shortcuts)
+    answers = [0] * nb_intersections
+    shortcuts_dict = to_dict(shortcuts)
+    print(shortcuts_dict)
+    for i in range(1, nb_intersections):
+        print(f"checking {i=}")
+        if i not in shortcuts_dict:
+            print(f"{i=} not an end point")
+            answers[i] = answers[i-1] + 1
+            print(f"{answers[i]=}")
+        else:
+            origins = shortcuts_dict[i] + [i-1]
+            origins.remove(i)
+            print(f"{origins=}")
+            answers[i] = min(answers[origin] for origin in origins) + 1
+            print(f"{answers[i]=}")
+            retro_check(answers, i)
+    return answers
+
+
+def to_dict(shortcuts):
+    """
+    Dict with origin point for each shortcut's ending.
+
+    All values are re-normalized to origin = 0.
+    """
+    shortcuts_dict = {}
+    for index, value in enumerate(shortcuts):
+        try:
+            shortcuts_dict[value-1].append(index)
+        except KeyError:
+            shortcuts_dict[value-1] = [index]
+    return shortcuts_dict
+
+
+def retro_check(answers, i):
+    if answers[i] <= answers[i-1] - 2:
+        print("retro-check needed")
+        print(f"{answers[i]=}")
+        print(f"before : {answers[i-1]=}")
+        answers[i-1] = answers[i] + 1
+        print(f"after : {answers[i-1]=}")
+        retro_check(answers, i-1)
+
+
+for arr in [arr1, arr2, arr3, arr4]:
+    print(solution(arr))
+    print("\n\n\n")
+
